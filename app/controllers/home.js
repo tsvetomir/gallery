6d334f634b71a6a6d91ae10af2f14fd1b976122e
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2015-05-29 02:05:59 +0300

    Resize images on the fly using sharp

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,6 +1,7 @@
 var config = require('../../config/config');
 var express = require('express');
 var fs = require('fs');
+var sharp = require('sharp');
 var path = require('path');
 
 var router = express.Router();
@@ -16,7 +17,7 @@ function unauthorized() {
 }
 
 router.param('id', function(req, res, next, id) {
-    var match = id.match(/[a-z0-9]{8}/i);
+    var match = id.match(/\w{8}/);
     if (match) {
         var root = match[0];
         var rootPath = path.join(config.img, match[0]);
@@ -24,12 +25,12 @@ router.param('id', function(req, res, next, id) {
             if (err) {
                 next(unauthorized());
             } else {
+                req.rootPath = rootPath;
                 req.files = files.filter(function(name) {
                     return path.extname(name).toLowerCase() in { ".jpeg": true, ".jpg": true };
                 }).map(function(name) {
                     return {
                         name: name,
-                        thumb: name.replace(path.extname(name), "_thumb.jpg"),
                         root: root
                     };
                 });
@@ -42,6 +43,17 @@ router.param('id', function(req, res, next, id) {
     }
 });
 
+router.param('name', function(req, res, next, name) {
+    var match = name.match(/^[\w\-. ]+$/);
+    if (match) {
+        req.name = match[0];
+        req.fileName = path.join(req.rootPath, req.name);
+        next();
+    } else {
+        next(new Error("Not found"));
+    }
+});
+
 router.get('/', function (req, res, next) {
     next(unauthorized());
 });
@@ -51,4 +63,29 @@ router.get('/:id', function (req, res, next) {
         title: 'Gallery',
         files: req.files
     });
+});
+
+router.get('/:id/:name', function (req, res, next) {
+    sharp(req.fileName)
+        .resize(1200, null)
+        .toBuffer(function(err, buffer, info) {
+            if (err) {
+                next(err);
+            }
+
+            res.send(buffer);
+        });
+});
+
+router.get('/:id/thumb/:name', function (req, res, next) {
+    sharp(req.fileName)
+        .resize(100)
+        .progressive()
+        .toBuffer(function(err, buffer, info) {
+            if (err) {
+                next(err);
+            }
+
+            res.send(buffer);
+        });
 });
